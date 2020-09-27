1c3d0b05133ca1f489b035f0a38c957e3c0c9685
Donghao Wu <donghao.wu.us@gmail.com>
2020-09-27 00:16:56 -0400

    finished graphql apollo readme, deleted all redux code and files in GraplQL-Apollo project folder.

--- GraphQL-Apollo/clothing-friends-graphql-apollo/client/src/redux/display-name/display-name.actions.js
+++
@@ -1,8 +0,0 @@
-import { SET_DISPLAY_NAME } from './display-name.types';
-
-export const setDisplayName = displayName => {
-    return {
-        type: SET_DISPLAY_NAME,
-        payload: displayName
-    }
-};
--- GraphQL-Apollo/clothing-friends-graphql-apollo/client/src/redux/display-name/display-name.selectors.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect';
-
-const selectDisplayName = state => state.displayName;
-
-export const selectInputDisplayName = createSelector(
-    [selectDisplayName],
-    displayName => displayName.input
-);
