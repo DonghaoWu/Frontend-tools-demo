851c0af434046f209dbbd9329af6dbc044a6d26c
Donghao Wu <donghao.wu.us@gmail.com>
2020-10-05 14:38:22 -0400

    add notification sagas and styles, fixed order styles

--- Firebase-Cart-Orders/clothing-friends-firebase-cart-orders/client/src/redux/notices/notices.actions.js
+++ Firebase-Cart-Orders/clothing-friends-firebase-cart-orders/client/src/redux/notices/notices.actions.js
@@ -1,47 +1,22 @@
-import { SET_NOTICE, REMOVE_NOTICE } from './notices.types';
+import {
+    ADD_NOTICE,
+    REMOVE_NOTICE
+} from './notices.types';
 
-import { v4 as uuid } from "uuid";
-
-// export const setNotice = (msg, noticeType) => dispatch => {
-//     console.log('setNotice is working now.======>')
-//     const id = uuid.v4();
-//     dispatch({
-//         type: SET_NOTICE,
-//         payload: {
-//             msg: msg,
-//             noticeType: noticeType,
-//             id: id
-//         }
-//     });
-
-//     setTimeout(() => dispatch({
-//         type: REMOVE_NOTICE,
-//         payload: id,
-//     }), 3000);
-// }
-// export const setNotice = () => {
-//     console.log('hello here =======>')
-//     // const id = uuid();
-//     return {
-//         type: SET_NOTICE,
-//         payload: {
-//             msg: 'success',
-//             noticeType: 'success',
-//             // id: id
-//         }
-//     }
-// };
-
-export const notice = (id) => ({
-    type: SET_NOTICE,
-    payload: {
-        msg: 'Login success',
-        noticeType: 'success',
-        id: id
+export const addNotice = (msg, noticeType, id) => {
+    return {
+        type: ADD_NOTICE,
+        payload: {
+            msg: msg,
+            noticeType: noticeType,
+            id: id
+        }
     }
-});
+}
 
-export const remove = (id) => ({
-    type: REMOVE_NOTICE,
-    payload: id
-});
+export const removeNotice = (id) => (
+    {
+        type: REMOVE_NOTICE,
+        payload: id
+    }
+);
--- Firebase-Cart-Orders/clothing-friends-firebase-cart-orders/client/src/redux/notices/notices.reducer.js
+++ Firebase-Cart-Orders/clothing-friends-firebase-cart-orders/client/src/redux/notices/notices.reducer.js
@@ -1,12 +1,12 @@
-import { SET_NOTICE, REMOVE_NOTICE } from './notices.types';
+import { ADD_NOTICE, REMOVE_NOTICE } from './notices.types';
 const initialState = [];
 
 export default function (state = initialState, action) {
     console.log('inside notices reducer');
     const { payload, type } = action;
     switch (type) {
-        case SET_NOTICE:
-        console.log('in this case');
+        case ADD_NOTICE:
+            console.log('in this case');
             return [...state, payload];
         case REMOVE_NOTICE:
             return state.filter(notice => notice.id !== payload);
